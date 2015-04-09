73fbfa632eda120d1b2efa5ec32fc5602c6dfdcf
Marcos Hernández <marcoslhc@gmail.com>
2015-04-09 13:14:43 -0400

    Fix local gulp

--- index.js
+++ index.js
@@ -2,28 +2,29 @@ var localGulp, gulp,
     resolve = require('resolve');
 
 try {
-    localGulp = resolve.sync('gulp', {basedir: process.cwd()});
-    gulp = require(localGulp);
+        localGulp = resolve.sync('gulp', {basedir: process.cwd()});
+        gulp = require(localGulp);
 } catch (e) {
-    console.log('No local gulp found. Use npm install gulp');
-    process.exit(1);
+        console.log('No local gulp found. Use npm install gulp');
+        process.exit(1);
 }
 
 function loadTasks(config) {
     var tasks = Object.keys(config);
+
     tasks.forEach(function (name) {
         var taskConfig = config[name],
             task = require(taskConfig.def)(taskConfig),
             deps =  taskConfig.deps || [];
 
         if (deps.length !== 0) {
-            localGulp.task(name, taskConfig.deps, task);
+            gulp.task(name, taskConfig.deps, task);
         } else {
-            localGulp.task(name, task);
+            gulp.task(name, task);
         }
     });
 
-    return localGulp;
+    return gulp;
 }
 
 module.exports = function (config) {
